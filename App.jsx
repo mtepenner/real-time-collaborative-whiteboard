d3beb2c0ac7f83aa91644b36e9d9fcd775da1047
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-02 10:52:01 -0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Whiteboard from './Whiteboard';
+
+function App() {
+  return (
+    <div className="App">
+      {/* You could add a toolbar component here later for colors/brush sizes */}
+      <Whiteboard />
+    </div>
+  );
+}
+
+export default App;
